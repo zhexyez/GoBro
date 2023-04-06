2d4c5594f0a9413f45babe1d2b4e1f16af0d5708
Eugene <roreugene@gmail.com>
2023-04-06 17:09:19 +0300

    progress

--- egoparser.go
+++ egoparser.go
@@ -19,6 +19,8 @@ import (
 	9: 32,  // \x20 (space)
 }*/
 
+const zero uint64 = 0
+
 func SPOT(egofile string) {
 	ego, err := os.Open(egofile)
 	if err != nil {
@@ -28,6 +30,14 @@ func SPOT(egofile string) {
 
 	page := NewPage()
 
+	var parent parent
+
+	var child_buffer []*element
+
+	var tree_buffer []*element
+
+	var parent_buffer []*element
+
 	scanner := bufio.NewScanner(ego)
 	scanner.Bytes()
 	for scanner.Scan() {
@@ -35,11 +45,12 @@ func SPOT(egofile string) {
 			log.Fatal(err)
 			return
 		}
-		var parent_buffer []*element
+
 		line := scanner.Text()
 		if len(line) <= 2 {
 			continue
 		}
+
 		for i := range line {
 			if i >= len(line)-2 {
 				continue
@@ -77,27 +88,65 @@ func SPOT(egofile string) {
 				continue
 			}
 			if string(line[i:i+3]) == "<e>" {
+				parent = nil
+				if len(parent_buffer) >= 1 {
+					fmt.Println("NOPROP all buffer when buffer >= 1", parent_buffer)
+					parent = parent_buffer[len(parent_buffer)-1]
+					fmt.Println("parent when buffer is >= 1", parent_buffer[len(parent_buffer)-1])
+				}
+				new_element := NewElement("std", "", parent, child_buffer, "")
+				fmt.Println("new without properties:", new_element, ": parent ->", new_element.PARENT, ": child ->", len(new_element.CHILD))
+				parent_buffer = append(parent_buffer, new_element)
+				fmt.Println("parent buffer when new: ", parent_buffer)
+				//fmt.Println("from <e> element added:", parent_buffer)
 				if i >= len(line)-3 {
 					continue
 				}
-				new_element := NewElement(string([]byte{}), string([]byte{}), nil, nil, string([]byte{}))
-				parent_buffer = append(parent_buffer, new_element)
 				i += 2
 			}
 			if string(line[i:i+3]) == "</>" {
-				if i >= len(line)-3 || len(parent_buffer) == 0 {
+				// fmt.Println("</>")
+				switch uint64(len(parent_buffer)) {
+				case zero:
 					continue
-				}
-				if len(parent_buffer) == 1 {
+				case zero + 1:
 					parent_buffer[0].ChangeParent(page)
-					//fmt.Println(string(MakeTree_inJSON(parent_buffer[0])))
-					fmt.Println(string(MakeTree_inJSON(parent_buffer[0])))
+					tree_buffer = append(tree_buffer, parent_buffer[0])
+					fmt.Println("tree buffer updated: ", tree_buffer)
 					parent_buffer = nil
-				} else {
+					fmt.Println("when tree updated, new buffer is ->", parent_buffer)
+				default:
+					//fmt.Println("</> and buffer 2 or more")
 					parent_buffer[len(parent_buffer)-1].ChangeParent(parent_buffer[len(parent_buffer)-2])
+					//fmt.Println("changed parent on:", parent_buffer[len(parent_buffer)-2])
 					parent_buffer[len(parent_buffer)-2].AppendChild(parent_buffer[len(parent_buffer)-1])
+					//fmt.Println("appended child from:", parent_buffer[len(parent_buffer)-1])
 					parent_buffer = parent_buffer[:len(parent_buffer)-1]
-					fmt.Println(parent_buffer)
+					fmt.Println("new parent when removed:", parent_buffer)
+					fmt.Println("tree buffer updated when removing parent buffer:", tree_buffer)
+				}
+				// TODO DELETE THIS
+				// if uint64(len(parent_buffer)) == zero {
+				// 	continue
+				// }
+				// if uint64(len(parent_buffer)) == zero+1 {
+				// 	fmt.Println("buffer 1")
+				// 	parent_buffer[0].ChangeParent(page)
+				// 	//fmt.Println(string(MakeTree_inJSON(parent_buffer[0])))
+				// 	tree_buffer = append(tree_buffer, parent_buffer[0])
+				// 	//fmt.Println(string(MakeTree_inJSON(tree_buffer[0])))
+				// 	parent_buffer = nil
+				// } else if  {
+				// 	fmt.Println("</> and buffer 2 or more")
+				// 	parent_buffer[len(parent_buffer)-1].ChangeParent(parent_buffer[len(parent_buffer)-2])
+				// 	fmt.Println("changed parent on:", parent_buffer[len(parent_buffer)-2])
+				// 	parent_buffer[len(parent_buffer)-2].AppendChild(parent_buffer[len(parent_buffer)-1])
+				// 	fmt.Println("appended child from:", parent_buffer[len(parent_buffer)-1])
+				// 	parent_buffer = parent_buffer[:len(parent_buffer)-1]
+				// 	//fmt.Println(parent_buffer)
+				// }
+				if i >= len(line)-3 {
+					continue
 				}
 				i += 2
 			}
@@ -151,13 +200,26 @@ func SPOT(egofile string) {
 						}
 					}
 				}
+				if len(charbuf_id) == 0 {
+					charbuf_id = []byte("std")
+				}
 				//fmt.Println(string(charbuf_id), string(charbuf_class), string(charbuf_ref))
-				new_element := NewElement(string(charbuf_id), string(charbuf_class), nil, nil, string(charbuf_ref))
-				//fmt.Println(new_element)
+				fmt.Println("parent buffer before new with properties:", parent_buffer)
+				if len(parent_buffer) >= 1 {
+					fmt.Println("PROP all buffer when buffer >= 1", parent_buffer)
+					parent = parent_buffer[len(parent_buffer)-1]
+					fmt.Println("parent when buffer is >= 1", parent_buffer[len(parent_buffer)-1])
+				}
+				new_element := NewElement(string(charbuf_id), string(charbuf_class), parent, child_buffer, string(charbuf_ref))
+				fmt.Println("new with properties:", new_element, ": parent ->", new_element.PARENT, ": child ->", new_element.CHILD)
 				parent_buffer = append(parent_buffer, new_element)
-				//fmt.Println(parent_buffer)
+				fmt.Println("parent when with properties:", parent_buffer)
 			}
 		}
 	}
-	//fmt.Println(string(MakeTree_inJSON(page)))
+	fmt.Println("length of tree buffer:", len(tree_buffer))
+	for el := range tree_buffer {
+		fmt.Println(string(MakeTree_inJSON(tree_buffer[el])))
+	}
+	fmt.Println("page:", string(MakeTree_inJSON(page)))
 }
